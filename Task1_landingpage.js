3b008ec991b569984f8ae90ecdc6519ff0aa4641
Satyabrata Sahoo <158853769+satyabratasahoo01@users.noreply.github.com>
2024-05-09 04:25:03 -0400

    Add files via upload

---
+++ Task1_landingpage.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const ctaBtn = document.getElementById('ctaBtn');
+
+    ctaBtn.addEventListener('click', function() {
+        alert('You clicked the button!');
+    });
+});
